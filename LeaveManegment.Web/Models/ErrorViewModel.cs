b11a3c60e263a56b424abcd195f94fa849884598
Admin <Admin@DESKTOP-8UI49VD>
2022-12-29 21:31:05 +0530

    Add project files.

---
+++ LeaveManegment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManegment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
